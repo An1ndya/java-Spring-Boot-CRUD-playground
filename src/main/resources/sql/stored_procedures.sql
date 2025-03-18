fdb1488550f3e3df8222576db631d96b2329d447
Anindya Biswas <anindya33046@gmail.com>
2025-03-18 14:02:51 +0600

    update sp

--- src/main/resources/sql/stored_procedures.sql
+++ src/main/resources/sql/stored_procedures.sql
@@ -2,18 +2,22 @@
 
 -- Procedure to insert a new employee
 DELIMITER //
-CREATE PROCEDURE sp_insert_employee(
+DROP PROCEDURE IF EXISTS sp_insert_employee;
+Create PROCEDURE sp_insert_employee(
     IN first_name_param VARCHAR(255),
     IN last_name_param VARCHAR(255),
     IN email_param VARCHAR(255),
     IN phone_number_param VARCHAR(20),
     IN position_param VARCHAR(255),
-    IN salary_param DECIMAL(10,2)
+    IN salary_param DECIMAL(10,2),
+    IN manager_id_param int 
 )
 BEGIN
-    INSERT INTO employees (first_name, last_name, email, phone_number, position, salary)
-    VALUES (first_name_param, last_name_param, email_param, phone_number_param, position_param, salary_param);
+    INSERT INTO employees (first_name, last_name, email, phone_number, position, salary, manager_id)
+    VALUES (first_name_param, last_name_param, email_param, phone_number_param, position_param, salary_param, manager_id_param);
     
     SELECT * FROM employees WHERE id = LAST_INSERT_ID();
 END //
 DELIMITER ;
+
+
